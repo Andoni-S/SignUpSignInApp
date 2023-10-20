a9859c7e570053fd6a9951bc7c1835a9ebaac1ac
sunder-012 <jagoba1207@gmail.com>
2023-10-20 10:55:13 +0200

    FEAT
    Created two methods in Pool

--- RetoSignInServidor/src/model/Pool.java
+++ RetoSignInServidor/src/model/Pool.java
@@ -6,24 +6,53 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.Stack;
 
 /**
  *
- * @author 2dam
+ * @author Jagoba Bartolomé Barroso
  */
 public class Pool {
 
     private ResourceBundle configFile;
     private String url, user, pass, driver;
     private static Stack<Connection> connectionStack = new Stack();
-            
-    public Pool() {
+    
+    /**
+     * 
+     * @throws ClassNotFoundException 
+     */
+    public Pool() throws ClassNotFoundException {
         configFile = ResourceBundle.getBundle("logicTier.config");
         url = configFile.getString("URL");
         user = configFile.getString("USER");
         pass = configFile.getString("PASSWORD");
         driver = configFile.getString("DRIVER");
+        
+        Class.forName(driver);
+    }
+    
+    /**
+     * 
+     * @return
+     * @throws SQLException 
+     */
+    public synchronized Connection getConnection() throws SQLException{
+        if (connectionStack.isEmpty()){
+            Connection con = DriverManager.getConnection(url, user, pass);
+            connectionStack.push(con);
+        }
+        return connectionStack.pop();
+    }
+    
+    /**
+     * 
+     * @param con 
+     */
+    public synchronized void saveConnection(Connection con){
+        connectionStack.push(con);
     }
 }
