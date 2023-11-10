e63a3b4a224b5f4ecf37871cfe2999641375387a
Ander Goirigolzarri Iturburu <ander.goiritur@gmail.com>
2023-11-10 08:56:58 +0100

    Libraty Javadoc finished

---
+++ Reto1Library/src/exceptions/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * Provides a set of custom exceptions for handling various scenarios in the program.
+ * <p>
+ * The exceptions in this package are designed to cover specific error
+ * conditions and provide meaningful information to aid in debugging and
+ * resolution.
+ * </p>
+ * <p>
+ * The package includes the following exceptions:
+ * <ul>
+ *      <li>{@link com.example.exceptions.ConfirmPasswordException ConfirmPasswordException}</li>
+ *      <li>{@link com.example.exceptions.CredentialsException CredentialsException}</li>
+ *      <li>{@link com.example.exceptions.EmailAlreadyExistsException EmailAlreadyExistsException}</li>
+ *      <li>{@link com.example.exceptions.EmailFormatException EmailFormatException}</li>
+ *      <li>{@link com.example.exceptions.MaxCharException MaxCharException}</li>
+ *      <li>{@link com.example.exceptions.NumericException NumericException}</li>
+ *      <li>{@link com.example.exceptions.PasswordFormatException PasswordFormatException}</li>
+ *      <li>{@link com.example.exceptions.ServerErrorException ServerErrorException}</li>
+ * </ul>
+ * </p>
+ *
+ * @author Jagoba Bartolomé
+ */
+package exceptions;
